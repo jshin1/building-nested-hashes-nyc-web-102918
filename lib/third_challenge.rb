485c4a88012387d8050eef4417b5b4cff4e95a2f
Ji S <j2yshin@gmail.com>
2018-10-17 23:28:39 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,10 +7,11 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name: "Romeo", age: "15", status: "alive"
+        name: "Lady Montague", age: "54"
         },
       :hero => {
         #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
